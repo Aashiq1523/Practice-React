d81ed8f83d03584f1c3878e3e24f45fa429ad9b8
Aashiq <aashiqmohameda@gmail.com>
2023-02-20 01:40:34 +0530

    state and props

--- myfirstreactapp/src/App.js
+++ myfirstreactapp/src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, This is using JSX</h1>
-    </div>
-    //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello, This is using React element calls'))
-  );
+class App extends Component {
+
+  // state
+  state = {
+    persons: [
+      { name: 'Aashiq', age: '26'},
+      { name: 'Soffi', age: '23'}
+    ]
+  };
+
+  // manipulating state
+  expandNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Mohamed Aashiq', age: '26'},
+        { name: 'Soffi Ashraf', age: '23'}
+      ]
+    });
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>Hello, This is using JSX</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I am an Optom</Person>
+        <button onClick={this.expandNameHandler}>ExpandName</button>
+      </div>
+    );
+  }
 }
 
 export default App;
