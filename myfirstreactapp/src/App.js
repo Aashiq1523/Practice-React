ba15c5a0af950a9c7422c88db35d074cf4c1bcce
Aashiq <aashiqmohameda@gmail.com>
2023-02-23 18:18:03 +0530

    conditional rendering preferred way

--- myfirstreactapp/src/App.js
+++ myfirstreactapp/src/App.js
@@ -48,25 +48,30 @@ const App = () => {
     margin: '8px'
   }
 
+  // preferred way for conditional rendering
+  let persons = null
+
+  if (showPerson) {
+    persons = (
+      <div>
+        <Person
+          name={person.persons[0].name}
+          age={person.persons[0].age}
+          change={nameChangeHandler}
+          />
+        <Person 
+          name={person.persons[1].name}
+          age={person.persons[1].age}>
+            I am an Optom
+        </Person>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <h1>Hello, This is using JSX</h1>
-
-      {/* Rendering Content Conditionally */
-        showPerson ?
-        <div>
-          <Person
-            name={person.persons[0].name}
-            age={person.persons[0].age}
-            change={nameChangeHandler}
-            />
-          <Person 
-            name={person.persons[1].name}
-            age={person.persons[1].age}>
-              I am an Optom
-          </Person>
-        </div> : null
-      }
+      {persons}
       <button style={style} onClick={togglePersonHandler}>Show Persons</button>
     </div>
   );
